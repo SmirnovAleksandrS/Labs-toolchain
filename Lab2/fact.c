ba2b83f82e9aa8f6d00758369085d3425e62e5e2
Smirnov Aleksandr <smirnov.aleksandr.s@phystech.edu>
2024-03-31 13:10:04 +0300

    remake structure Lab1 and add root Lab2

---
+++ Lab2/fact.c
@@ -0,0 +1,9 @@
+
+int counter = 0;
+
+unsigned fact(unsigned x) {
+  if (x < 2)
+    return 1;
+
+  return x * fact(x - 1);
+}
---
+++ Lab2/main.c
@@ -0,0 +1,8 @@
+
+#include <stdio.h>
+
+int main() {
+  unsigned f = fact(5);
+  printf("%u\n", f);
+  return 0;
+}
